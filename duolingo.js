e5da9b963b4ba1b72e84a904c5b94fa34d3eb4ce
poloniculmov <github@poloniculmov.com>
2015-07-09 10:10:09 +0300

    Update duolingo.js
    Moved to MutationObserver

--- duolingo.js
+++ duolingo.js
@@ -2,16 +2,28 @@
 // @name         My Fancy New Userscript
 // @namespace    http://poloniculmov.com
 // @version      0.1
-// @description  Hides golden courses.
+// @description  enter something useful
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js
-// @author       Poloniculmov
+// @author       You
 // @match        https://www.duolingo.com/
 // @grant        none
 // ==/UserScript==
 
 this.$ = this.jQuery = jQuery.noConflict(true);
-$(document).ready(function()
-{    
+MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+
+var observer = new MutationObserver(function(mutations, observer) {
     $('.gold').parent().parent().hide();
     $('.row-shortcut').hide();   
 });
+
+var element = document.querySelector('body');
+observer.observe(element, {
+    childList: true,
+    characterData: true,
+    attributes: true,
+});
+
+$('.gold').parent().parent().hide();
+$('.row-shortcut').hide();   
+
